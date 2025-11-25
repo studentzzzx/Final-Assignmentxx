d42511f173c027be2256d08af88c8ed4dc693b0f
studentzzzx <zzzstudenta@gmail.com>
2025-11-26 03:25:51 +0700

    Create Student.cs

---
+++ CodeFirstDemo/Student.cs
@@ -0,0 +1,9 @@
+namespace CodeFirstDemo
+{
+    // POCO class representing the Student entity
+    public class Student
+    {
+        public int StudentId { get; set; }   // Primary Key
+        public string StudentName { get; set; }
+    }
+}
